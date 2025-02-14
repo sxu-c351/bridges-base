0b117c86b73f92cbaac3e3fa3940326b21fd9a30
James Vanderhyde <vanderhyde@sxu.edu>
2025-02-13 19:51:22 -0600

    Starter code for BRIDGES projects

--- BridgesBase/src/bridgesbase/BridgesBase.java
+++ BridgesBase/src/bridgesbase/BridgesBase.java
@@ -1,32 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+//Starter code for BRIDGES projects (https://bridgesuncc.github.io)
+//Created by James Vanderhyde, 13 February 2025
 
 package bridgesbase;
 
-import edu.sxu.cs.geometry.*;
+import bridges.base.SymbolCollection;
+import bridges.connect.Bridges;
+import bridges.connect.DataSource;
 
-/**
- *
- * @author jv4
- */
 public class BridgesBase 
 {
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) 
     {
-        //Test code for distance formulas
-        Point p1 = new Point(0,4);
-        Point p2 = new Point(3,0);
-        System.out.println(Geometry.distance(p1, p2) + " should be 5");
+        //Create the BRIDGES object with credentials.
+        //  If you are doing a BRIDGES assignment, use the assignment number. 
+        //  Otherwise, use any number.
+        //  Use your user name and API key from your BRIDGES registation.
+        Bridges bridges = new Bridges(ASSIGNMENT, USER_NAME, API_KEY); //do not commit your API key!
         
-        Line l1 = new Line(new Point(0,0), new Point(10,10));
-        System.out.println(Geometry.signedDistanceUnnormalized(p1, l1));
-        System.out.println(Geometry.signedDistanceUnnormalized(p2, l1));
-        System.out.println(Geometry.signedDistance(p1, l1));
-        System.out.println(Geometry.signedDistance(p2, l1));
+        //Set some information for the BRIDGES object
+        bridges.setTitle("Title of the project");
+	bridges.setDescription("Long description");
+
+        //Create the data connection object
+        DataSource ds = bridges.getDataSource();
+        
+        //Use the DataSource object to download a data set from the server
+        
+        //Read the data set into a data structure
+        
+        //Process the data
+        
+        //Create a scene graph (SymbolCollection) object
+        SymbolCollection cvh = new SymbolCollection();
+        
+        //Add shapes to the SymbolCollection for the server to draw
+
+        //Activate the visualization on the server  
+        try 
+        {
+            bridges.visualize();
+        } 
+        catch (java.io.IOException | bridges.validation.RateLimitException e) 
+        {
+            System.err.println(e);
+        }
     }
 }

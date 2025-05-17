76c82b759b82e81b64ec621dac899b1f3bd509b6
James Vanderhyde <vanderhyde@sxu.edu>
2025-05-17 09:56:17 -0500

    Latitude/Longitude to miles

--- BridgesStarter/src/edu/sxu/cs/geometry/Geometry.java
+++ BridgesStarter/src/edu/sxu/cs/geometry/Geometry.java
@@ -30,4 +30,25 @@ public final class Geometry
         return (l.b.y-l.a.y)*p.x - (l.b.x-l.a.x)*p.y + l.b.x*l.a.y - l.b.y*l.a.x;
     }
 
+    public static Point lonLatToMiles(Point p, Point origin)
+    {
+        //source of formulas: https://gis.stackexchange.com/questions/142326/calculating-longitude-length-in-miles
+        double f = 1/298.257223563;
+        double a = 6378137;
+        double e2 = (2-f)*f;
+        
+        double phi = origin.y; //latitude
+        double d = 1 - e2*Math.sin(phi);
+        double m = a*(1-e2)/(d*Math.sqrt(d));
+        double r = a*Math.cos(phi)/Math.sqrt(d);
+        
+        double oneDegLatInMiles = m/180*Math.PI/1609.34;
+        double oneDegLonInMiles = r/180*Math.PI/1609.34;
+        
+        double deltaLat = origin.y-p.y;
+        double deltaLon = origin.x-p.x;
+        
+        return new Point((float)(oneDegLonInMiles*deltaLon), (float)(oneDegLatInMiles*deltaLat));
+    }
+    
 }
